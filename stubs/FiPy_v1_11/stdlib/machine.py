073a1cbc800d68506bdca89e1ba9191a23fd9dae
Ludovic Jozeau <unifai@protonmail.com>
2022-11-23 00:13:23 +0100

    machine.Timer stubs

--- stubs/FiPy_v1_11/stdlib/machine.py
+++ stubs/FiPy_v1_11/stdlib/machine.py
@@ -567,11 +567,139 @@ class SPI:
 
 
 class Timer:
-    ''
-    Alarm = None
-    Chrono = None
-    def sleep_us():
-        pass
+    '''
+    Measure Time and Set Alarms
+
+    Timers can be used for a great variety of tasks, like measuring time spans or being notified that a specific interval has elapsed.
+
+    These two concepts are grouped into two different subclasses:
+
+    - Chrono: used to measure time spans.
+    - Alarm: to get interrupted after a specific interval.
+
+    You can create as many of these objects as needed.
+    '''
+    class Alarm:
+        '''
+        Example:
+
+        ```python
+        from machine import Timer
+
+        class Clock:
+
+        def __init__(self):
+            self.seconds = 0
+            self.__alarm = Timer.Alarm(self._seconds_handler, 1, periodic=True)
+
+        def _seconds_handler(self, alarm):
+            self.seconds += 1
+            print("%02d seconds have passed" % self.seconds)
+            if self.seconds == 10:
+                alarm.cancel() # stop counting after 10 seconds
+
+        clock = Clock()
+        ```
+        --------
+        For more information on how Pycom’s products handle interrupts, see [notes](https://docs.pycom.io/firmwareapi/notes/#interrupt-handling).
+        '''
+
+        def __init__(self, handler, s: float | None = None, ms: int | None = None, us: int | None = None, arg=None, periodic=False):
+            '''
+            Create an Alarm object.
+
+            - `handler` : will be called after the interval has elapsed. If set to `None` , the alarm will be disabled after creation.
+            - `arg` : an optional argument can be passed to the callback handler function. If `None` is specified, the function will receive the object that triggered the alarm.
+            - `s, ms, us` : the interval can be specified in seconds (float), miliseconds (integer) or microseconds (integer). Only one at a time can be specified.
+            - `periodic` : an alarm can be set to trigger repeatedly by setting this parameter to `True` .
+            '''
+            ...
+
+        def callback(self, handler, arg=None):
+            '''
+            Specify a callback handler for the alarm. If set to `None` , the alarm will be disabled.
+
+            An optional argument `arg` can be passed to the callback handler function. If `None` is specified, the function will receive the object that triggered the alarm.
+            '''
+            ...
+
+        def cancel(self):
+            '''
+            Disables the alarm.
+            '''
+            ...
+
+    class Chrono:
+        '''
+        Example:
+
+        ```python
+        from machine import Timer
+        import time
+
+        chrono = Timer.Chrono()
+
+        chrono.start()
+        time.sleep(1.25) # simulate the first lap took 1.25 seconds
+        lap = chrono.read() # read elapsed time without stopping
+        time.sleep(1.5)
+        chrono.stop()
+        total = chrono.read()
+
+        print()
+        print("the racer took %f seconds to finish the race" % total)
+        print("  %f seconds in the first lap" % lap)
+        print("  %f seconds in the last lap" % (total - lap))
+        ```
+        '''
+
+        def __init__(self):
+            '''
+            Create a chronometer object.
+            '''
+            ...
+        def start(self):
+            '''
+            Start the chronometer.
+            '''
+            ...
+
+        def stop(self):
+            '''
+            Stop the chronometer.
+            '''
+            ...
+
+        def reset(self):
+            '''
+            Reset the time count to 0.
+            '''
+            ...
+
+        def read(self) -> float:
+            '''
+            Get the elapsed time in seconds.
+            '''
+            ...
+
+        def read_ms(self) -> float:
+            '''
+            Get the elapsed time in milliseconds.
+
+            '''
+            ...
+
+        def read_us(self) -> float:
+            '''
+            Get the elapsed time in microseconds.
+            '''
+            ...
+
+    def sleep_us(self, us: int):
+        '''
+        Delay for a given number of microseconds, should be positive or 0 (for speed, the condition is not enforced). Internally it uses the same timer as the other elements of the `Timer` class. It compensates for the calling overhead, so for example, 100us should be really close to 100us. For times bigger than 10,000us it releases the GIL to let other threads run, so exactitude is not guaranteed for delays longer than that.
+        '''
+        ...
 
 
 class Touch:
