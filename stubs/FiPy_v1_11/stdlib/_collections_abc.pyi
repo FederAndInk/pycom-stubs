9cf05a7d5eb9bda9b3c7fde2993fe23104d9fd7f
Ludovic Jozeau <unifai@protonmail.com>
2022-11-23 00:12:55 +0100

    add collections stdlib stubs

---
+++ stubs/FiPy_v1_11/stdlib/_collections_abc.pyi
@@ -0,0 +1,54 @@
+from typing import AbstractSet as Set
+from typing import AsyncGenerator as AsyncGenerator
+from typing import AsyncIterable as AsyncIterable
+from typing import AsyncIterator as AsyncIterator
+from typing import Awaitable as Awaitable
+from typing import ByteString as ByteString
+from typing import Callable as Callable
+from typing import Collection as Collection
+from typing import Container as Container
+from typing import Coroutine as Coroutine
+from typing import Generator as Generator
+from typing import Hashable as Hashable
+from typing import ItemsView as ItemsView
+from typing import Iterable as Iterable
+from typing import Iterator as Iterator
+from typing import KeysView as KeysView
+from typing import Mapping as Mapping
+from typing import MappingView as MappingView
+from typing import MutableMapping as MutableMapping
+from typing import MutableSequence as MutableSequence
+from typing import MutableSet as MutableSet
+from typing import Reversible as Reversible
+from typing import Sequence as Sequence
+from typing import Sized as Sized
+from typing import ValuesView as ValuesView
+
+# Without the real definition, mypy and pytest both think that __all__ is empty, so re-exports nothing
+__all__ = [
+    "Awaitable",
+    "Coroutine",
+    "AsyncIterable",
+    "AsyncIterator",
+    "AsyncGenerator",
+    "Hashable",
+    "Iterable",
+    "Iterator",
+    "Generator",
+    "Reversible",
+    "Sized",
+    "Container",
+    "Callable",
+    "Collection",
+    "Set",
+    "MutableSet",
+    "Mapping",
+    "MutableMapping",
+    "MappingView",
+    "KeysView",
+    "ItemsView",
+    "ValuesView",
+    "Sequence",
+    "MutableSequence",
+    "ByteString",
+]
