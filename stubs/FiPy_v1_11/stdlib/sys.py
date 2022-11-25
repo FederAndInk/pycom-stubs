1680c2896f6f5af0e857dd528390c797c0eedca2
Ludovic Jozeau <unifai@protonmail.com>
2022-11-25 23:43:25 +0100

    sys stubs

--- stubs/FiPy_v1_11/stdlib/sys.py
+++ stubs/FiPy_v1_11/stdlib/sys.py
@@ -3,24 +3,45 @@ Module: 'sys' on FiPy v1.11
 """
 # MCU: (sysname='FiPy', nodename='FiPy', release='1.20.2.r6', version='v1.11-c5a0a97 on 2021-10-28', machine='FiPy with ESP32', lorawan='1.0.2', sigfox='1.0.1', pybytes='1.7.1')
 # Stubber: 1.3.2
-argv = None
-byteorder = 'little'
-def exc_info():
+
+from types import ModuleType, TracebackType
+from typing import Any, Literal, NamedTuple, Union
+
+from uio import FileIO
+
+argv: list
+byteorder: Literal["little", "big"]
+
+_ExcInfo = tuple[type[BaseException], BaseException, TracebackType]
+_OptExcInfo = Union[_ExcInfo, tuple[None, None, None]]
+
+
+def exc_info() -> _OptExcInfo:
+    ...
+
+
+def exit(retval=0):
     pass
 
-def exit():
-    pass
 
-implementation = None
+class _Implementation(NamedTuple):
+    name: str
+    version: tuple[int, int, int]
+
+
+implementation: _Implementation
 maxsize = 2147483647
-modules = None
-path = None
+modules: dict[str, ModuleType]
+path: list[str]
 platform = 'FiPy'
-def print_exception():
-    pass
 
-stderr = None
-stdin = None
-stdout = None
+
+stderr: FileIO = Any
+stdin: FileIO = Any
+stdout: FileIO = Any
 version = '3.4.0'
 version_info = (3, 4, 0)
+
+
+def print_exception(exc: BaseException, file=stdout):
+    ...
